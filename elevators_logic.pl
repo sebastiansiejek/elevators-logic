6741a3220e2ecf5fb42a8231322d1309d1143471
Sebastian Siejek <siejeksebastian@gmail.com>
2021-01-20 18:46:33 +0100

    feat: add sample elevators and floor mooves

--- elevators_logic.pl
+++ elevators_logic.pl
@@ -2,6 +2,7 @@
 :- use_module(library(pce)).
 :- use_module(library(tabular)).
 
+
 run:-
 	initial().
 
@@ -26,13 +27,35 @@ initial():-
                   ]),
 	send(new(D, dialog), below, Pict),
     send(D, append, button('Krok', message(@prolog, step))),
-    send(D, append, button('Reset', message(@prolog, clear))),
 	send(new(E, dialog), below, Pict),
     send(E, append, label(reporter)),
 	send(Pict, open).
 
-step():-
-    send(reporter, 'Step').
+% Step 1: 1 & 3 up
+% Step 2: 4 & 5 up
+% Step 3: 4 & 5 down
+% Step 4: 2 & 5 up
+% Step 5: 2 & 5 down
+% Step 6: 3 & 5 up
+% Step 7: 3 & 5 down
+% Step 8: 4 & 5 up
 
-clear() :-
-    write('Clear program').
+setFloor(X, X1):-
+    X is X1.
+
+upFloor(X, X1):-
+    X1 is X+8.
+
+downFloor(X, X1):-
+    X1 is X-13.
+
+step1(X1, X3, R1, R2):-
+    upFloor(X1, R1),
+    upFloor(X3, R2).
+
+start(X1, X2, X3, X4, X5):-
+    setFloor(X1, 17),
+    setFloor(X2, 26),
+    setFloor(X3, 20),
+    setFloor(X4, 19),
+    setFloor(X5, 31).
